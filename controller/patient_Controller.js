3ce6b53008e89057452176d464bb82156c98a7c4
Sayyadsulthan <asulthan088@gmail.com>
2023-06-06 20:35:48 +0530

    some bugs fixed

--- controller/patient_Controller.js
+++ controller/patient_Controller.js
@@ -3,35 +3,35 @@ const Patient = require("../models/patient")
 const Report = require("../models/report")
 
 // for register of user by doctor 
-module.exports.register = async function(req, res){
-    try{
-        let doctor =await Doctor.findOne({name:req.user.name, password: req.user.password})
-        let patient = await Patient.findOne({name:req.body.name, phone:req.body.phone}).populate('report')
+module.exports.register = async function (req, res) {
+    try {
+        let doctor = await Doctor.findOne({ name: req.user.name, password: req.user.password })
+        let patient = await Patient.findOne({ name: req.body.name, phone: req.body.phone }).populate('report')
 
         // if patient is find by name and number 
-        if(patient){
+        if (patient) {
             return res.status(200).json({
-                message:"patient already exist..",
+                message: "patient already exist..",
                 patient: patient
             })
         }
 
         // else create new patient
         let newpatient = await Patient.create({
-             name:req.body.name,
-            phone:req.body.phone
+            name: req.body.name,
+            phone: req.body.phone
         })
 
         doctor.patient.push(newpatient.id)
 
         doctor.save();
-        
+
         return res.status(201).json({
-            message:'patient registration successfull...',
+            message: 'patient registration successfull...',
             patient: newpatient
         })
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
         return res.status(500).json({
             message: "Internal Server Error",
@@ -41,65 +41,61 @@ module.exports.register = async function(req, res){
 }
 
 // CREATING THE REPORT USING THIS 
-module.exports.create_report =async function(req, res){
-    try{
-        let doctor =await Doctor.findOne({name:req.user.name, password: req.user.password})
-        let patient = await Patient.findById( req.params.id)
+module.exports.create_report = async function (req, res) {
+    try {
+        let doctor = await Doctor.findOne({ name: req.user.name, password: req.user.password })
+        let patient = await Patient.findById(req.params.id)
 
-        if(!patient){
+        if (!patient) {
             return res.status(404).json({
-                message:"Patient not found....",
+                message: "Patient not found....",
                 id: req.params.id
-               })
+            })
         }
 
-            // find the report if it exists or not
-            let report = await Report.findOne({patient: req.params.id});
-            if(report ){
+        // find the report if it exists or not
+        let report = await Report.findOne({ patient: req.params.id });
+        if (report) {
 
-                if(report.createdby == doctor.name){
-                    return res.status(200).json({
-                        message:"Report already Exist...",
-                        report: report
-                    })
-                } 
-            }
-            
-            // CHECKING THE STATUS OF REPORT
-            let status;
-            
-                if(req.body.status =='Positive-Admit' || req.body.status == 'Travelled-Quarantine' || req.body.status== 'Symptoms-Quarantine'){
-                        status =req.body.status;
-                }else{
-                   status= 'Negative'
-                } 
-
-                // new date date object to store the date of created report
-                let date = new Date();
-                let mm = date.getMonth()+1, dd= date.getDate(), yy= date.getFullYear()
-                let currentDate= dd+"/"+mm+"/"+yy;
-
-                let newreport= await Report.create({
-                    status: status,
-                    createdby: doctor.name,
-                    Date: currentDate,
-                    patient: patient
+            if (report.createdby == doctor.name) {
+                return res.status(200).json({
+                    message: "Report already Exist...",
+                    report: report
                 })
-            
-           patient.report.push(newreport);
+            }
+        }
 
-           patient.save();
+        // CHECKING THE STATUS OF REPORT
+        let status;
 
-           return res.status(200).json({
-               message:"Report created successfully..",
-               report: newreport
-            })
-        
-        
-        
+        if (req.body.status == 'Positive-Admit' || req.body.status == 'Travelled-Quarantine' || req.body.status == 'Symptoms-Quarantine') {
+            status = req.body.status;
+        } else {
+            status = 'Negative'
+        }
 
+        // new date date object to store the date of created report
+        let date = new Date();
+        let mm = date.getMonth() + 1, dd = date.getDate(), yy = date.getFullYear()
+        let currentDate = dd + "/" + mm + "/" + yy;
 
-    }catch(err){
+        let newreport = await Report.create({
+            status: status,
+            createdby: doctor.name,
+            Date: currentDate,
+            patient: patient
+        })
+
+        patient.report.push(newreport.id);
+
+        patient.save();
+
+        return res.status(200).json({
+            message: "Report created successfully..",
+            report: newreport
+        })
+
+    } catch (err) {
         console.log(err)
         return res.status(500).json({
             message: "Internal Server Error",
@@ -110,32 +106,32 @@ module.exports.create_report =async function(req, res){
 
 
 // TO DISPLAY THE ALL REPORTS OF THE SPECIFIC PATIENT
-module.exports.all_Reports = async function(req, res){
-    try{
-        let patient = await Patient.findById( req.params.id).populate('report').exec()
+module.exports.all_Reports = async function (req, res) {
+    try {
+        let patient = await Patient.findById(req.params.id).populate('report').exec()
 
         // if patient do not exist 
-        if(!patient){
+        if (!patient) {
             return res.status(404).json({
-                message:" patient not found You are looking for.."
+                message: " patient not found You are looking for.."
             })
         }
 
-        let report = patient.report; 
+        let report = patient.report;
 
         // if the reports are empty
-        if(!report){
-            report =" No records found... "
+        if (!report) {
+            report = " No records found... "
         }
 
         // return the response in json
         return res.status(200).json({
-            message:`all reports of ${patient.name}..`,
+            message: `all reports of ${patient.name}..`,
             reports: report
-         })
+        })
 
-        
-    }catch(err){
+
+    } catch (err) {
         console.log(err)
         return res.status(500).json({
             message: "Internal Server Error",
